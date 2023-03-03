697a6c4e5e05fd4e2a0ae1dff60289ecefc52056
antonio <antonio.cassano984@gmail.com>
2023-03-03 21:21:10 +0000

    Using lombok in Employee.java

--- src/main/java/net/javaguides/springboot/model/Employee.java
+++ src/main/java/net/javaguides/springboot/model/Employee.java
@@ -2,13 +2,14 @@ package net.javaguides.springboot.model;
 
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-@Entity
-@Table(name = "employees")
+@Getter @Setter
+@Entity @Table(name = "employees")
 public class Employee {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
 	@Column(name = "first_name")
@@ -19,28 +20,4 @@ public class Employee {
 	
 	@Column(name = "email")
 	private String email;
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
 }
